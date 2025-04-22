98327e5d813ea14d03ec6b36fa3bd6a1d69da134
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2025-04-22 22:02:49 +0530

    Java Assignment

--- JA-111-B17/Day-2-Java-Fundamentals-Assignment/PrimeFactorial.java
+++ JA-111-B17/Day-2-Java-Fundamentals-Assignment/PrimeFactorial.java
@@ -32,3 +32,41 @@ class Factorial
 	
 	}
 }
+
+
+// ```java
+// // Method 2
+// import java.util.Scanner;
+// class Score 
+// {
+// 	int one, two, three, four, six, totalRunScored;
+
+// 	public void totalScored(int one,int two,int three,int four,int six){
+// 		this.totalRunScored = one + two + three + four + six;
+// 		System.out.println(this.totalRunScored);
+// 	}
+
+// 	public static void main(String[] args) 
+// 	{
+// 		Score s = new Score();
+// 		Scanner myInput = new Scanner(System.in);
+
+// 		System.out.println("Enter ones");
+// 		s.one = myInput.nextInt();
+
+// 		System.out.println("Enter twos");
+// 		s.two = myInput.nextInt();
+
+// 		System.out.println("Enter three");
+// 		s.three = myInput.nextInt();
+
+// 		System.out.println("Enter four");
+// 		s.four = myInput.nextInt();
+
+// 		System.out.println("Enter six");
+// 		s.six = myInput.nextInt();
+
+// 		s.totalScored(s.one, s.two, s.three, s.four, s.six);
+// 	}
+// }
+// ```
