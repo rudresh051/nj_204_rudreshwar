95e5ec70fb610a9d33d99823cb4580c495371065
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2025-05-26 22:54:49 +0530

    Question 1 solved

--- JA-111-B17/Day-4/Student.java
+++ JA-111-B17/Day-4/Student.java
@@ -7,7 +7,17 @@ public class Student
 	private int marks;
 
 	// No argument constructor
-	Student(){}
+	Student(){
+	}
+
+	// Parameterized Constructor
+	Student(int roll, String name, int age, int marks){
+		this.roll = roll;
+		this.name = name;
+		setAge(age);
+		setMarks(marks);
+	}
+
 
 	//public getters
 	public int getRoll(){
@@ -31,18 +41,34 @@ public class Student
 		this.name = name;
 	}
 	public void setAge(int age){
-		this.age = age;
+		if(age > 18 && age < 60){
+			this.age = age;
+		}else{
+			System.out.println("Invalid age");
+		}
+		
 	}
 	public void setMarks(int marks){
-		this.marks = marks;
+		if(marks > 0 && marks < 500){
+				this.marks = marks;
+		}else{
+			System.out.println("Invalid Marks");
+		}
 	}
 
 	public static void main(String[] args) 
 	{
 		// object1
 		Student stud1 = new Student();
+		stud1.setRoll(25);
+		stud1.setName("abcd");
+		stud1.setAge(22);
+		stud1.setMarks(400);
 
 		// object2
-		Student stud2 = new Student();
+		Student stud2 = new Student(2, "efgh", 25, 480);
+
+		System.out.println(stud1.getRoll() + " " + stud1.getName() + " " + stud1.getAge() + " " + stud1.getMarks());
+		System.out.println(stud2.getRoll() + " " + stud2.getName() + " " + stud2.getAge() + " " + stud2.getMarks());
 	}
 }
